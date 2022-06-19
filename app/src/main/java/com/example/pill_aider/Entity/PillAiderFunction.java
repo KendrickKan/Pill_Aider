09c379832e24e8c50426153e14cac813d727b5d6
KendrickKan <kandong@njust.edu.cn>
2022-06-19 13:54:46 +0800

    PillAiderFuction(一些有用小方法3)

--- app/src/main/java/com/example/pill_aider/Entity/PillAiderFunction.java
+++ app/src/main/java/com/example/pill_aider/Entity/PillAiderFunction.java
@@ -3,6 +3,7 @@ package com.example.pill_aider.Entity;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class PillAiderFunction {
     //eg: h=10,m=30 -> String:"10:30"
@@ -71,22 +72,88 @@ public class PillAiderFunction {
     public static String getTodayDate() {
         String TodayDate = String.valueOf(LocalDate.now());
         List<Integer> list = stringToThreeDate(TodayDate);
-        TodayDate = ThreeDateToString(list.get(0),list.get(1),list.get(2));
+        TodayDate = ThreeDateToString(list.get(0), list.get(1), list.get(2));
         return TodayDate;
     }
+
+
+    // Reminder to String:"1-1-1-1-1-1-1-TEST"
+    public static String reminderToString(Reminder r) {
+        String str="";
+        if (r != null)
+             str = String.valueOf(r.getItem_id()) + "-" +
+                    r.getItem_name() + "-" +
+                    String.valueOf(r.getNum_day()) + "-" +
+                    String.valueOf(r.getDasage_per_time()) + "-" +
+                    String.valueOf(r.getItem_type()) + "-" +
+                    String.valueOf(r.getItem_time()) + "-" +
+                    String.valueOf(r.getItem_rem()) + "-" +
+                    r.getNotice();
+        else str = "NONE";
+        return str;
+    }
+
+    public static Reminder stringToReminder(String str){
+        if (Objects.equals(str, "NONE")){
+            Reminder r = new Reminder("1",1,1,1,1,1,"记得多喝热水");
+            r.setItem_id(1);
+            return r;
+        }
+        String id="";
+        String name="";
+        String Num_day="";
+        String Dasage_per_time="";
+        String Item_type="";
+        String Item_time="";
+        String Item_rem="";
+        String Notice="";
+        int flag = 1;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == '-') {
+                flag++;
+                continue;
+            }
+            if (flag == 1) {
+                id += str.charAt(i);
+            } else if (flag == 2) {
+                name += str.charAt(i);
+            } else if (flag == 3) {
+                Num_day += str.charAt(i);
+            } else if (flag == 4) {
+                Dasage_per_time += str.charAt(i);
+            } else if (flag == 5) {
+                Item_type += str.charAt(i);
+            } else if (flag == 6) {
+                Item_time += str.charAt(i);
+            } else if (flag == 7) {
+                Item_rem += str.charAt(i);
+            } else {
+                Notice += str.charAt(i);
+            }
+        }
+        Reminder r = new Reminder(name,
+                Integer.parseInt(Num_day),
+                Integer.parseInt(Dasage_per_time),
+                Integer.parseInt(Item_type),
+                Integer.parseInt(Item_time),
+                Integer.parseInt(Item_rem),
+                Notice
+                );
+        r.setItem_id(Integer.parseInt(id));
+        return r;
+    }
+
 //
 //    public static void main(String[] args) {
-////        String str = "10:01";
-////        int a,b;
-////        List<Integer> list = stringToTwoTime(str);
-////        for(int i = 0;i<list.size();i++) {
-////            System.out.println(list.get(i));
-////        }
-////        a=list.get(0);
-////        b=list.get(1);
-////        System.out.println(twoTimeToString(b,a));
-//
-////        System.out.println(getTodayDate());
+//        String str = reminderToString(null);
+//        System.out.println(str);
+//        Reminder r = stringToReminder(str);
+//        System.out.println(r.getNotice());
+//        r.setNotice("TEST");
+//        str = reminderToString(r);
+//        System.out.println(str);
+//        r=stringToReminder(str);
+//        System.out.println(r.getNotice());
 //    }
 
 }
